63e5f3f56032eab8a4844b9cc3f8e9d73bbb882f
Fortunato Mugnano <fortunato.mugnano88@gmail.com>
2019-08-26 21:18:43 -0500

    create a new repository

---
+++ src/scripts/journal.js
@@ -0,0 +1,31 @@
+console.log("My daily journal is working");
+
+const journalEntries = [{
+    dateOfEntry: "26 August 2019",
+    conceptsCovered: "Objects and Functions in JS",
+    entry: "Today we have learned about Objects and how important they are for JS, also we have discussed about the Functions and their value",
+    mood: "ok"
+}];
+
+
+journalEntries.push({
+    dateOfEntry: "23 August 2019",
+    conceptsCovered: "Celebrity Project Presentation",
+    entry: "We have presented our first project in front of the class, it was good!",
+    mood: "super"
+})
+
+journalEntries.push({
+    dateOfEntry: "22 August 2019",
+    conceptsCovered: "Project work",
+    entry: "We have worked as a team in the first school project",
+    mood: "ok"
+})
+
+journalEntries.push({
+    dateOfEntry: "12 August 2019",
+    conceptsCovered: "First day of school",
+    entry: "Today it was the first day of school, I was very exited to meet new people and start this adventure!",
+    mood: "super"
+})
+console.log(journalEntries);
