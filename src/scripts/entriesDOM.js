9c94adbfdf15ae42867729c50b22825385eec92e
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-09-03 10:53:16 -0500

    created the final object inside the entiesDom js file

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,12 +1,12 @@
-const cretePhrase = document.querySelector(".entryLog") // Make a variable to add something in the DOM
-
-
 const journalEntries = {
- addToJournal(dataArray) {   // This function will attach the variable in the HTML
-    dataArray.forEach(element => {
-        let entryHtml = createEntrie.makeJournalEntryComponent(element)
-        cretePhrase.innerHTML += entryHtml
-    });
-}
+    addToJournal(dataArray) {   // This function will attach the variable in the HTML
+        dataArray.forEach(element => {
+            let entryHtml = createEntrie.makeJournalEntryComponent(element)
+            entriesDom.cretePhrase.innerHTML += entryHtml
+        });
+    }
 }
 
+const entriesDom = {
+    cretePhrase: document.querySelector(".entryLog"),
+}
